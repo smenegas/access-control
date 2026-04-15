1ad5d68b1168ed743dc8b84ae3aed86c7afd0c3b
smenegas <suianmenegas@gmail.com>
2026-04-14 21:19:15 -0300

    Necessary routes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import ClaimantDashboard from './components/claimant-dashboard/claimant-dashboard';
 import Login from './components/login/login';
+import NewAccount from './components/new-account/new-account';
+import AdminPanel from './components/admin/admin-panel';
 import './App.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<ClaimantDashboard />} />
+        <Route path="/new-account" element={<NewAccount />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
   );
